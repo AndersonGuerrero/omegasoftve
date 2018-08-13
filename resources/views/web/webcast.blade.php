4f046f6f0fe3a4bc5d11c7401cbde71e18573a81
victor <victorvec3@gmail.com>
2018-08-13 16:33:29 -0400

    webcast

--- resources/views/web/webcast.blade.php
+++ resources/views/web/webcast.blade.php
@@ -7,14 +7,58 @@
         </div>
     </section>
 
-    <section class="">
+    <section>
         <div class="auto-container">
             <div class="row">
                 <div class="col-md-12 col-sm-12 col-xs-12" style="margin-top: 40px;">
                     <div class="sec-title centered" style="margin-bottom: 30px">
-                        <h1 style="font-size: 30px">Conozca las principales funcionalidades que ofrece <br> nuestro aplicativo</h1>
-                        <div class="text">Participe en nuestros Webcast en línea, interactúe con nuestros especialistas y descubra las soluciones que <br> traemos para ustedes.</div>
+                        <h1 style="font-size: 30px">Conozca las principales funcionalidades que ofrece nuestro aplicativo</h1>
+                        <div class="text">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Ducimus quam doloremque sequi id debitis voluptatum illum 
+                            porro asperiores nostrum, qui ad deleniti, laboriosam cum 
+                            saepe temporibus architecto nobis commodi expedita!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Iusto possimus impedit, magni laudantium in deleniti odit 
+                            nisi eos ipsa saepe dolores velit voluptates sapiente molestiae 
+                            praesentium architecto amet neque sed.
+                        </div>
                     </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="col-md-6 col-sm-6 col-xs-6">
+                    <div class="sec-title centered" style="margin-bottom: 30px;border-style: solid;">
+                        <div class="text" style="font-size: 30px;">
+                            OmegaITSM, Potencia y simplicidad en una misma herramienta.
+                        </div>
+                        <div class="inner-box" style="background: #212165; height: 250px;">
+                            <figure  class="image-box"><a href=""><img src="http://localhost:8000/images/resource/" alt=""></a></figure>
+                        </div>
+                        <div class="inner-box" style="background: #212165;height: 60px; margin-top: 15px">
+                            <figure  class="image-box"><a href=""><img src="http://localhost:8000/images/resource/" alt=""></a></figure>
+                        </div>
+                        <div class="text" style="font-size: 17px; text-align: initial;color: black; margin-bottom: 70px;padding: 15px;">
+                                Duración: 45 min Aprox - Español
+                        </div>
+                        <div class="text" style="font-size: 20px; text-align: initial;padding: 15px;">
+                                <b>Contenido</b>
+                        </div>
+                        <div class="text" style="text-align: justify; padding: 15px;">
+                                Para brindar una atención efectiva y en los
+                                tiempos establecidos, Omega ITSM ofrece a su
+                                organización un control centralizado de las
+                                solicitudes de soporte y garantiza la
+                                disponibilidad en las operaciones del negocio,
+                        </div>
+                       <div style="height: 70px;background: #2976f5;">
+                           <h1 style="color:#fff; padding-top: 20px; font-size: 30px">Registrate</h1>
+                       </div>
                 </div>
             </div>
         </div>
