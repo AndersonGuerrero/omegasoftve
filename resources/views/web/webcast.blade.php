2aced50fba61eca0e82da127df88917dad7c05ab
victor <victorvec3@gmail.com>
2018-08-13 16:37:56 -0400

    webcast

--- resources/views/web/webcast.blade.php
+++ resources/views/web/webcast.blade.php
@@ -59,8 +59,38 @@
                        <div style="height: 70px;background: #2976f5;">
                            <h1 style="color:#fff; padding-top: 20px; font-size: 30px">Registrate</h1>
                        </div>
+                    </div>
                 </div>
-            </div>
+                <div class="col-md-6 col-sm-6 col-xs-6">
+                        <div class="sec-title centered" style="margin-bottom: 30px;border-style: solid;">
+                            <div class="text" style="font-size: 30px;">
+                                OmegaITSM, Potencia y simplicidad en una misma herramienta.
+                            </div>
+                            <div class="inner-box" style="background: #212165; height: 250px;">
+                                <figure  class="image-box"><a href=""><img src="http://localhost:8000/images/resource/" alt=""></a></figure>
+                            </div>
+                            <div class="inner-box" style="background: #212165;height: 60px; margin-top: 15px">
+                                <figure  class="image-box"><a href=""><img src="http://localhost:8000/images/resource/" alt=""></a></figure>
+                            </div>
+                            <div class="text" style="font-size: 17px; text-align: initial;color: black; margin-bottom: 70px;padding: 15px;">
+                                    Duración: 45 min Aprox - Español
+                            </div>
+                            <div class="text" style="font-size: 20px; text-align: initial;padding: 15px;">
+                                    <b>Contenido</b>
+                            </div>
+                            <div class="text" style="text-align: justify; padding: 15px;">
+                                    Para brindar una atención efectiva y en los
+                                    tiempos establecidos, Omega ITSM ofrece a su
+                                    organización un control centralizado de las
+                                    solicitudes de soporte y garantiza la
+                                    disponibilidad en las operaciones del negocio,
+                            </div>
+                           <div style="height: 70px;background: #2976f5;">
+                               <h1 style="color:#fff; padding-top: 20px; font-size: 30px">Registrate</h1>
+                           </div>
+                        </div>
+                    </div>
+            </div>   
         </div>
     </section>
 @endsection
