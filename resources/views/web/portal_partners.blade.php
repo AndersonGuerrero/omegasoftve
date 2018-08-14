964db8313752a97ac5e418fc0e50993286c47135
victor <victorvec3@gmail.com>
2018-08-14 10:00:54 -0400

    portar panter

--- resources/views/web/portal_partners.blade.php
+++ resources/views/web/portal_partners.blade.php
@@ -6,14 +6,23 @@
     <div class="image-layer" style="background-image:url({{ URL::to('/') }}/images/background/fondo-para-portal-de-partner.jpg); opacity: 10.0;"></div>
     <div class="auto-container">
         <div class="row">
-            <div class="col- md-4 col-sm-4 col-xs-12">
-                    <figure class="image-box"><a href=""><img style="padding: 80px;" src="{{ URL::to('/') }}/images/resource/solicitud-de-seriales.png" alt=""></a></figure>
+            <div class="col- md-4 col-sm-4 col-xs-12" style="padding: 100px;">
+                <figure class="image-box"><a href=""><img src="{{ URL::to('/') }}/images/resource/solicitud-de-seriales.png" alt=""></a></figure>
+                <div class="sec-title centered">
+                    <h1 style="font-size: 25px;color: #fff;margin-top: -20px;">Registro y seguimients de oportunidades</h1>
+                </div>
             </div>
-            <div class="col- md-4 col-sm-4 col-xs-12">
-                    <figure class="image-box"><a href=""><img style="padding: 80px;" src="{{ URL::to('/') }}/images/resource/pruebas-beta-2.png" alt=""></a></figure>
+            <div class="col- md-4 col-sm-4 col-xs-12" style="padding: 100px;">
+                <figure class="image-box"><a href=""><img src="{{ URL::to('/') }}/images/resource/pruebas-beta-2.png" alt=""></a></figure>
+                <div class="sec-title centered">
+                    <h1 style="font-size: 25px;color: #fff;margin-top: -20px;">Definir</h1>
+                </div>
             </div>
-            <div class="col- md-4 col-sm-4 col-xs-12">
-                    <figure class="image-box"><a href=""><img style="padding: 80px;" src="{{ URL::to('/') }}/images/resource/oportunidades-de-negocio-2.png" alt=""></a></figure>
+            <div class="col- md-4 col-sm-4 col-xs-12" style="padding: 100px;">
+                <figure class="image-box"><a href=""><img src="{{ URL::to('/') }}/images/resource/oportunidades-de-negocio-2.png" alt=""></a></figure>
+                <div class="sec-title centered">
+                    <h1 style="font-size: 25px;color: #fff;margin-top: -20px;">Definir</h1>
+                </div>
             </div>
         </div>
     </div>
