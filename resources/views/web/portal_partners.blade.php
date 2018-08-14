08c93f3f3c4184c2e2c42721d50ddd4d88e3f234
victor <victorvec3@gmail.com>
2018-08-14 10:02:08 -0400

    portar panter

--- resources/views/web/portal_partners.blade.php
+++ resources/views/web/portal_partners.blade.php
@@ -7,13 +7,13 @@
     <div class="auto-container">
         <div class="row">
             <div class="col- md-4 col-sm-4 col-xs-12" style="padding: 100px;">
-                <figure class="image-box"><a href=""><img src="{{ URL::to('/') }}/images/resource/solicitud-de-seriales.png" alt=""></a></figure>
+                <figure class="image-box"><a href=""><img src="{{ URL::to('/') }}/images/resource/pruebas-beta-2.png" alt=""></a></figure>
                 <div class="sec-title centered">
                     <h1 style="font-size: 25px;color: #fff;margin-top: -20px;">Registro y seguimients de oportunidades</h1>
                 </div>
             </div>
             <div class="col- md-4 col-sm-4 col-xs-12" style="padding: 100px;">
-                <figure class="image-box"><a href=""><img src="{{ URL::to('/') }}/images/resource/pruebas-beta-2.png" alt=""></a></figure>
+                <figure class="image-box"><a href=""><img src="{{ URL::to('/') }}/images/resource/solicitud-de-seriales.png" alt=""></a></figure>
                 <div class="sec-title centered">
                     <h1 style="font-size: 25px;color: #fff;margin-top: -20px;">Definir</h1>
                 </div>
