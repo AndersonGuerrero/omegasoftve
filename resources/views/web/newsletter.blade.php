1587fa2dd047ba7d026442ea4a68dbac9d4c4bbb
victor <victorvec3@gmail.com>
2018-08-23 14:13:46 -0400

    ITSM

--- resources/views/web/newsletter.blade.php
+++ resources/views/web/newsletter.blade.php
@@ -7,47 +7,50 @@
         </div>
     </section>
 
-    <section>
+    <section style="padding-bottom: 70px">
             <div class="auto-container">
                 <div class="row">
                     <div class="col-md-12 col-sm-12 col-xs-12" style="margin-top: 40px;">
-                        <div class="sec-title centered" style="text-align: initial; margin-bottom: 30px">
-                            <h1>Acerca de Nosotros</h1>
-                            <div class="text">Omegasoft nace en el 2010 como una empresa de servicios IT, 
-                            Integrada con un grupo de profecionales experimentados y preparados en el área
-                            de gestion servicios IT, ofreciendo a las organazaciones, servicios profecionales
-                            para mejorar sus capacidades organizativas, mejorar las prestación de servicio
-                            por parte de su centro de soporte, redicir tiempos y mejorar la eficiencia.</div>
+                        <div class="sec-title centered" style="text-align: center; margin-bottom: 30px">
+                            <h1>Manténgase informado de nuestras novedades</h1>
+                            <div class="text" style="font-size: 25px;" >
+                                En nuestros boletenes mensuales encontraras información relevante <br> 
+                                sobre nuestros productos, lanzamientos de productos, <br> actualizaciones 
+                                de versión y próximos eventos.
+                            </div>
                         </div>
                     </div>
                     <div style="height: 100px;"> 
                         <div style="height: 70px; background: #fff">
-                            <div class="col-md-3 col-sm-12 col-xs-12">
-                                <div class="sec-title centered">
-                                    <div class="text" style="text-align: initial;">Suscribase al Newsletter de ManageEngine</div>
-                                </div>
-                            </div>
-                            <div class="col-md-3 col-sm-12 col-xs-12" style="margin-top: 23px;">
-                            {{ Form::open(array('route' => 'newsletter.send', 'id' => 'form-newsletter-form')) }}
-                                <div class="input-group mb-3">
-                                    <input
-                                    name="email" 
-                                    type="email" class="form-control" 
-                                    placeholder="Email ID" aria-label="Email" 
-                                    aria-describedby="button-addon2">
-                                    <div class="input-group-append">
-                                        <button 
-                                        data-callback="send_newsletterForm"
-                                        data-sitekey="6LcjmmgUAAAAAIpIHH-NyYnEJwI8xhRB2knImJDW"
-                                        style="background: green; color: white; font-size: 15px;" 
-                                        class="btn btn-outline-secondary g-recaptcha" type="button">Enviar</button>
-                                    </div>
-                                </div>
-                            @include('flash::message')
-                            {{ Form::close() }}
-                            </div>
                             <div class="col-md-6 col-sm-12 col-xs-12">
                                 <div class="sec-title centered">
+                                    <div class="text" style="text-align: center;">
+                                        <h1 style="font-size: 30px;padding-top: 12px;">Suscríbase a nuestros boletines</h1>
+                                    </div>
+                                </div>
+                            </div>
+                           
+                            <div class="col-md-6 col-sm-12 col-xs-12">
+                                <div class="sec-title centered">
+                                        <div class="" style="margin-top: 23px;">
+                                                {{ Form::open(array('route' => 'newsletter.send', 'id' => 'form-newsletter-form')) }}
+                                                    <div class="input-group mb-3">
+                                                        <input
+                                                        name="email" 
+                                                        type="email" class="form-control" 
+                                                        placeholder="Email ID" aria-label="Email" 
+                                                        aria-describedby="button-addon2">
+                                                        <div class="input-group-append">
+                                                            <button 
+                                                            data-callback="send_newsletterForm"
+                                                            data-sitekey="6LcjmmgUAAAAAIpIHH-NyYnEJwI8xhRB2knImJDW"
+                                                            style="background: green; color: white; font-size: 15px;" 
+                                                            class="btn btn-outline-secondary g-recaptcha" type="button">Enviar</button>
+                                                        </div>
+                                                    </div>
+                                                @include('flash::message')
+                                                {{ Form::close() }}
+                                                </div>
                                     <div class="text" style="text-align: initial;">Nota:Nunca venderemos o compatiremos su direccion de correo electronico</div>
                                 </div>
                             </div>
@@ -57,11 +60,11 @@
             </div>
     </section>
 
-    <section class="news-section-two">
+    <!--<section class="news-section-two">
             <div class="auto-container">
                 
                 <div class="row clearfix">
-                    <!--News Block-->
+               
                     <div class="news-block-two col-md-4 col-sm-6 col-xs-12">
                         <div class="inner-box">
                             <figure class="image-box"><img src="{{ URL::to('/') }}/images/resource/image-13.jpg" alt=""></figure>
@@ -73,7 +76,7 @@
                             </div>
                         </div>
                     </div>
-                    <!--News Block-->
+                  
                     <div class="news-block-two col-md-4 col-sm-6 col-xs-12">
                         <div class="inner-box">
                             <figure class="image-box"><a href="blog-detail.html"><img src="{{ URL::to('/') }}/images/resource/image-14.jpg" alt=""></a></figure>
@@ -85,7 +88,7 @@
                             </div>
                         </div>
                     </div>
-                    <!--News Block-->
+                    
                     <div class="news-block-two col-md-4 col-sm-6 col-xs-12">
                         <div class="inner-box">
                             <figure class="image-box"><a href="blog-detail.html"><img src="{{ URL::to('/') }}/images/resource/image-15.jpg" alt=""></a></figure>
@@ -99,5 +102,5 @@
                     </div>
                 </div>
             </div>
-    </section>
+    </section>-->
 @endsection
