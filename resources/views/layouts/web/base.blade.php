efd40933687cab330a1787d7ffa5c7c848fc0072
anderson2811 <anderson.andres.guerrero@gmail.com>
2018-08-31 10:57:25 -0400

    fixs

--- resources/views/layouts/web/base.blade.php
+++ resources/views/layouts/web/base.blade.php
@@ -100,7 +100,9 @@
 
     var CaptchaLoad = function() {
         var data = {'sitekey': '6LcjmmgUAAAAAIpIHH-NyYnEJwI8xhRB2knImJDW'}
-        window.id_captcha_footer = grecaptcha.render($('#g-recaptcha-footer')[0], data);
+        if($('#g-recaptcha-footer')[0]){
+            window.id_captcha_footer = grecaptcha.render($('#g-recaptcha-footer')[0], data);
+        }
         
         if($('#g-recaptcha-webcast')[0]){
             window.id_captcha_webcast = grecaptcha.render($('#g-recaptcha-webcast')[0], data);
@@ -121,6 +123,11 @@
         if($('#g-recaptcha-partner')[0]){
             window.id_captcha_partner = grecaptcha.render($('#g-recaptcha-partner')[0], data);
         }
+
+        if($('#g-recaptcha-quotation')[0]){
+            window.id_captcha_quotation = grecaptcha.render($('#g-recaptcha-quotation')[0], data);
+        }
+        
     };
 
     $('.btn-send').on('click', function (event) {
@@ -139,6 +146,8 @@
                 grecaptcha.execute(window.id_captcha_team);
             }else if(idcaptcha === 'g-recaptcha-partner'){
                 grecaptcha.execute(window.id_captcha_partner);
+            }else if(idcaptcha === 'g-recaptcha-quotation'){
+                grecaptcha.execute(window.id_captcha_quotation);
             }
         }else{
             $(event.currentTarget).closest('form')[0].reportValidity()
